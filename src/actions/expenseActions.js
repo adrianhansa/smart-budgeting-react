287b241d002ad3aacc6340217b07cec0ed8859f0
Adrian Hansa <adrian.hansa@yahoo.com>
2021-12-11 11:50:46 +0000

    Created all expenses related actions.

--- src/actions/expenseActions.js
+++ src/actions/expenseActions.js
@@ -25,6 +25,17 @@ export const addExpense =
   async (dispatch) => {
     try {
       dispatch({ type: ADD_EXPENSE_REQUEST });
+      const { data } = await axios.post("http://localhost:5000/exenses", {
+        amount,
+        date,
+        description,
+        account,
+      });
+      dispatch({ type: ADD_EXPENSE_SUCCESS, payload: data });
+      const result = await axios.get(
+        `http://localhost:5000/exenses/${month}/${year}`
+      );
+      dispatch({ type: GET_EXPENSES_SUCCESS, payload: result.data });
     } catch (error) {
       dispatch({
         type: ADD_EXPENSE_FAIL,
@@ -35,3 +46,102 @@ export const addExpense =
       });
     }
   };
+
+export const updateExpense =
+  (id, { amount, date, description, account }) =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: UPDATE_EXPENSE_REQUEST });
+      const { data } = await axios.put(`http://localhost:5000/exenses/${id}`, {
+        amount,
+        date,
+        description,
+        account,
+      });
+      dispatch({ type: UPDATE_EXPENSE_SUCCESS, payload: data });
+      const result = await axios.get(
+        `http://localhost:5000/exenses/${month}/${year}`
+      );
+      dispatch({ type: GET_EXPENSES_SUCCESS, payload: result.data });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_EXPENSE_FAIL,
+        payload:
+          error.response.data.message && error.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+export const getExpense = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_EXPENSE_REQUEST });
+    const { data } = await axios.get(`http://localhost:5000/exenses/${id}`);
+    dispatch({ type: GET_EXPENSE_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: GET_EXPENSE_FAIL,
+      payload:
+        error.response.data.message && error.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const deleteExpense = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: DELETE_EXPENSE_REQUEST });
+    const { data } = await axios.delete(`http://localhost:5000/exenses/${id}`);
+    dispatch({ type: DELETE_EXPENSE_SUCCESS, payload: data });
+    const result = await axios.get(
+      `http://localhost:5000/exenses/${month}/${year}`
+    );
+    dispatch({ type: GET_EXPENSES_SUCCESS, payload: result.data });
+  } catch (error) {
+    dispatch({
+      type: DELETE_EXPENSE_FAIL,
+      payload:
+        error.response.data.message && error.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getExpenses = () => async (dispatch) => {
+  try {
+    dispatch({ type: GET_EXPENSES_REQUEST });
+    const result = await axios.get(
+      `http://localhost:5000/exenses/${month}/${year}`
+    );
+    dispatch({ type: GET_EXPENSES_SUCCESS, payload: result.data });
+  } catch (error) {
+    dispatch({
+      type: GET_EXPENSES_FAIL,
+      payload:
+        error.response.data.message && error.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getExpensesByMonthAndYear = (month, year) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_EXPENSES_BY_MONTH_AND_YEAR_REQUEST });
+    const { data } = await axios.get(
+      `http://localhost:5000/exenses/${month}/${year}`
+    );
+    dispatch({ type: GET_EXPENSES_BY_MONTH_AND_YEAR_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: GET_EXPENSES_BY_MONTH_AND_YEAR_FAIL,
+      payload:
+        error.response.data.message && error.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
