edb38617b3c9be110dfe84f49d775cfa23efd281
Adrian Hansa <adrian.hansa@yahoo.com>
2021-12-13 19:40:33 +0000

    Styling Login and Register forms.

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../actions/userActions";
+import { Form, Col, Row, Container, Button, Card } from "react-bootstrap";
 import * as yup from "yup";
 import { Formik } from "formik";
 import { useNavigate } from "react-router-dom";
@@ -20,43 +21,64 @@ const Login = () => {
   }, [user, navigate]);
   const dispatch = useDispatch();
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      <Formik
-        validationSchema={validationSchema}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => {
-          dispatch(login(values));
-        }}
-      >
-        {(props) => {
-          return (
-            <>
-              <input
-                type="email"
-                value={props.values.email}
-                onChange={props.handleChange("email")}
-                onBlur={props.handleBlur("email")}
-                placeholder="Email"
-              />
-              {props.touched && <p>{props.errors.email}</p>}
-              <input
-                type="password"
-                value={props.values.password}
-                onChange={props.handleChange("password")}
-                onBlur={props.handleBlur("password")}
-                placeholder="Password"
-              />
-              {props.touched && <p>{props.errors.password}</p>}
-              <button type="submit" onClick={props.handleSubmit}>
-                Login
-              </button>
-            </>
-          );
-        }}
-      </Formik>
-    </div>
+    <Container fluid>
+      <Row className="mt-3">
+        <Col className="mx-auto" xs={12} sm={8} md={6} lg={3} xl={3}>
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+          <Card>
+            <Card.Header>
+              <h3 className="text-center">Login</h3>
+            </Card.Header>
+            <Card.Body>
+              <Formik
+                validationSchema={validationSchema}
+                initialValues={{ email: "", password: "" }}
+                onSubmit={(values) => {
+                  dispatch(login(values));
+                }}
+              >
+                {(props) => {
+                  return (
+                    <Form>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                          type="email"
+                          value={props.values.email}
+                          onChange={props.handleChange("email")}
+                          onBlur={props.handleBlur("email")}
+                          placeholder="Email"
+                        />
+                        {props.touched && <p>{props.errors.email}</p>}
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                          type="password"
+                          value={props.values.password}
+                          onChange={props.handleChange("password")}
+                          onBlur={props.handleBlur("password")}
+                          placeholder="Password"
+                        />
+                        {props.touched && <p>{props.errors.password}</p>}
+                      </Form.Group>
+                      <Button
+                        size="lg"
+                        type="submit"
+                        onClick={props.handleSubmit}
+                      >
+                        Login
+                      </Button>
+                    </Form>
+                  );
+                }}
+              </Formik>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/user/Register.js
+++ src/pages/user/Register.js
@@ -4,6 +4,7 @@ import * as yup from "yup";
 import { register } from "../../actions/userActions";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { Form, Col, Row, Container, Button, Card } from "react-bootstrap";
 
 const validationSchema = yup.object({
   name: yup.string().required("Please enter your name."),
@@ -23,73 +24,104 @@ const Register = () => {
     }
   }, [navigate, user]);
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      <Formik
-        initialValues={{
-          name: "",
-          household: "",
-          email: "",
-          password: "",
-          passwordVerify: "",
-        }}
-        onSubmit={(values) => {
-          dispatch(register(values));
-        }}
-        validationSchema={validationSchema}
-      >
-        {(props) => {
-          return (
-            <>
-              <input
-                type="email"
-                value={props.values.email}
-                onChange={props.handleChange("email")}
-                onBlur={props.handleBlur("email")}
-                placeholder="Email"
-              />
-              {props.touched && <p>{props.errors.email}</p>}
-              <input
-                type="text"
-                value={props.values.name}
-                onChange={props.handleChange("name")}
-                onBlur={props.handleBlur("name")}
-                placeholder="Name"
-              />
-              {props.touched && <p>{props.errors.name}</p>}
-              <input
-                type="text"
-                value={props.values.household}
-                onChange={props.handleChange("household")}
-                onBlur={props.handleBlur("household")}
-                placeholder="household"
-              />
-              {props.touched && <p>{props.errors.household}</p>}
-              <input
-                type="password"
-                value={props.values.password}
-                onChange={props.handleChange("password")}
-                onBlur={props.handleBlur("password")}
-                placeholder="Password"
-              />
-              {props.touched && <p>{props.errors.password}</p>}
-              <input
-                type="password"
-                value={props.values.passwordVerify}
-                onChange={props.handleChange("passwordVerify")}
-                onBlur={props.handleBlur("passwordVerify")}
-                placeholder="Verify password"
-              />
-              {props.touched && <p>{props.errors.password}</p>}
-              <button type="button" onClick={props.handleSubmit}>
-                Register
-              </button>
-            </>
-          );
-        }}
-      </Formik>
-    </div>
+    <Container fluid>
+      <Row className="mt-3">
+        <Col className="mx-auto" xs={12} sm={8} md={6} lg={3} xl={3}>
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+          <Card>
+            <Card.Header>
+              <h3 className="text-center">Register</h3>
+            </Card.Header>
+            <Formik
+              initialValues={{
+                name: "",
+                household: "",
+                email: "",
+                password: "",
+                passwordVerify: "",
+              }}
+              onSubmit={(values) => {
+                dispatch(register(values));
+              }}
+              validationSchema={validationSchema}
+            >
+              {(props) => {
+                return (
+                  <Card.Body>
+                    <Form>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                          type="email"
+                          value={props.values.email}
+                          onChange={props.handleChange("email")}
+                          onBlur={props.handleBlur("email")}
+                          placeholder="Email"
+                        />
+                        {props.touched && <p>{props.errors.email}</p>}
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          value={props.values.name}
+                          onChange={props.handleChange("name")}
+                          onBlur={props.handleBlur("name")}
+                          placeholder="Name"
+                        />
+                        {props.touched && <p>{props.errors.name}</p>}
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Household</Form.Label>
+                        <Form.Control
+                          type="text"
+                          value={props.values.household}
+                          onChange={props.handleChange("household")}
+                          onBlur={props.handleBlur("household")}
+                          placeholder="household"
+                        />
+                        {props.touched && <p>{props.errors.household}</p>}
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                          type="password"
+                          value={props.values.password}
+                          onChange={props.handleChange("password")}
+                          onBlur={props.handleBlur("password")}
+                          placeholder="Password"
+                        />
+                        {props.touched && <p>{props.errors.password}</p>}
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Re-type Password</Form.Label>
+                        <Form.Control
+                          type="password"
+                          value={props.values.passwordVerify}
+                          onChange={props.handleChange("passwordVerify")}
+                          onBlur={props.handleBlur("passwordVerify")}
+                          placeholder="Verify password"
+                        />
+                        {props.touched && <p>{props.errors.password}</p>}
+                      </Form.Group>
+
+                      <Button
+                        type="submit"
+                        size="lg"
+                        onClick={props.handleSubmit}
+                      >
+                        Register
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                );
+              }}
+            </Formik>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
